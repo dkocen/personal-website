65c202ed061e86c5eddf95ec87e5cae0847b65f2
David Kocen <dkocen0@gmail.com>
2023-09-20 13:41:49 -0700

    removed requirement for inter and katex

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import '@/css/extra.css'
-import 'katex/dist/katex.css'
-import '@fontsource/inter/variable-full.css'
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 import Router from 'next/router'
